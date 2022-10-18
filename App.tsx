6029697f67703511980dce2059d2a7089b928bec
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-10-18 14:28:19 +0300

    linter added

--- App.tsx
+++ App.tsx
@@ -7,61 +7,56 @@ import { Asset } from 'expo-asset';
 
 import JoinScreen from './src/screens/join-screen/join-screen';
 
-
 function cacheImages(images: any[]): Promise<any>[] {
-    return images.map((image) => {
-        if (typeof image === 'string') {
-            return Image.prefetch(image);
-        } else {
-            return Asset.fromModule(image).downloadAsync();
-        }
-    });
+  return images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
 function cacheFonts(fonts: any[]): Promise<void>[] {
-    return fonts.map((font) => Font.loadAsync(font));
+  return fonts.map((font) => Font.loadAsync(font));
 }
 
 export default function App() {
-    const [appIsReady, setAppIsReady] = useState(false);
-    useEffect(() => {
-        async function loadResourcesAndDataAsync() {
-            try {
-                SplashScreen.preventAutoHideAsync();
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
 
-                const imageAssets = cacheImages([
-                    require('./src/assets/join-screen-image.png'),
-                ]);
+        const imageAssets = cacheImages([require('./src/assets/join-screen-image.png')]);
 
-                const fontAssets = cacheFonts([
-                    { FontSource: '@expo-google-fonts/work-sans' },
-                    { FontSource: '@expo-google-fonts/rubik' },
-                ]);
+        const fontAssets = cacheFonts([
+          { FontSource: '@expo-google-fonts/work-sans' },
+          { FontSource: '@expo-google-fonts/rubik' },
+        ]);
 
-                await Promise.all([...imageAssets, ...fontAssets]);
-            } catch (e: any) {
-                // You might want to provide this error information to an error reporting service
-                console.warn(e);
-            } finally {
-                setAppIsReady(true);
-                SplashScreen.hideAsync();
-            }
-        }
-
-        loadResourcesAndDataAsync();
-    }, []);
-
-    if (!appIsReady) {
-        return null;
+        await Promise.all([...imageAssets, ...fontAssets]);
+      } catch (e: any) {
+        // You might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        SplashScreen.hideAsync();
+      }
     }
 
-    return (
-        <React.StrictMode>
-            <View>
-                <JoinScreen />
-            </View>
-        </React.StrictMode>
-    );
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View>
+      <JoinScreen />
+    </View>
+  );
 }
 
 // const styles = StyleSheet.create({
--- src/components/text-styled/text-styled.tsx
+++ src/components/text-styled/text-styled.tsx
@@ -2,29 +2,24 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
 type Props = {
-    text: string;
-    fontFamily?: string;
-    fontSize: number;
-    lineHeight: number;
-    color: string;
-    letterSpacing?: number;
-    textDecorationLine?:
-        | 'none'
-        | 'underline'
-        | 'line-through'
-        | 'underline line-through'
-        | undefined;
-    textAlign?: 'auto' | 'left' | 'right' | 'center' | 'justify' | undefined;
+  text: string;
+  fontFamily?: string;
+  fontSize: number;
+  lineHeight: number;
+  color: string;
+  letterSpacing?: number;
+  textDecorationLine?: 'none' | 'underline' | 'line-through' | 'underline line-through' | undefined;
+  textAlign?: 'auto' | 'left' | 'right' | 'center' | 'justify' | undefined;
 };
 
 const TextStyled: React.FC<Props> = ({ text, ...restProps }) => {
-    const styles = StyleSheet.create({
-        text: {
-            ...restProps,
-        },
-    });
+  const styles = StyleSheet.create({
+    text: {
+      ...restProps,
+    },
+  });
 
-    return <Text style={styles.text}>{text}</Text>;
+  return <Text style={styles.text}>{text}</Text>;
 };
 
 export default TextStyled;
